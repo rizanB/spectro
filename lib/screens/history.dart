71bc347cf84a739b3c32ee7d1ba6abc15ece21e6
rizanb <rijann5@gmail.com>
2024-12-30 04:43:37 +0545

    feat: add sort in history

--- lib/screens/history.dart
+++ lib/screens/history.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:spectro/screens/experiment_details.dart';
 
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
@@ -12,6 +13,7 @@ class HistoryScreen extends StatefulWidget {
 
 class _HistoryScreenState extends State<HistoryScreen> {
   List<Map<String, dynamic>> experiments = [];
+  String? _selectedSortOption = 'Date: recent first'; // Default sort by Date: recent first
 
   @override
   void initState() {
@@ -30,6 +32,7 @@ class _HistoryScreenState extends State<HistoryScreen> {
               .map((e) => jsonDecode(e) as Map<String, dynamic>)
               .toList(); // Parse JSON back into a list
         });
+        _sortExperiments(_selectedSortOption!); // Sort after loading data
       }
     } catch (e) {
       // Handle potential errors, e.g., JSON decoding issues
@@ -40,94 +43,126 @@ class _HistoryScreenState extends State<HistoryScreen> {
     }
   }
 
+  void _sortExperiments(String criteria) {
+    setState(() {
+      if (criteria == 'Date: recent first') {
+        experiments.sort((a, b) {
+          final dateA = DateTime.parse(a['date']);
+          final dateB = DateTime.parse(b['date']);
+          return dateB.compareTo(dateA); // Sort by date, latest first
+        });
+      } else if (criteria == 'Date: oldest first') {
+        experiments.sort((a, b) {
+          final dateA = DateTime.parse(a['date']);
+          final dateB = DateTime.parse(b['date']);
+          return dateA.compareTo(dateB); // Sort by date, oldest first
+        });
+      } else if (criteria == 'Name: a-z') {
+        experiments.sort((a, b) {
+          final nameA = a['name']?.toString().toLowerCase() ?? '';
+          final nameB = b['name']?.toString().toLowerCase() ?? '';
+          return nameA.compareTo(nameB); // Sort by name, A-Z
+        });
+      } else if (criteria == 'Name: z-a') {
+        experiments.sort((a, b) {
+          final nameA = a['name']?.toString().toLowerCase() ?? '';
+          final nameB = b['name']?.toString().toLowerCase() ?? '';
+          return nameB.compareTo(nameA); // Sort by name, Z-A
+        });
+      }
+      _selectedSortOption = criteria; // Update selected option
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('History'),
       ),
-      body: experiments.isEmpty
-          ? const Center(
-              child: Text('No experiments saved yet'),
-            )
-          : ListView.builder(
-              itemCount: experiments.length,
-              itemBuilder: (context, index) {
-                final experiment = experiments[index];
-                final date = DateTime.parse(experiment['date']);
-                final formattedDate =
-                    DateFormat('yyyy-MM-dd HH:mm').format(date);
-
-                return Card(
-                  child: ListTile(
-                    title: Text(
-                      experiment['name']?.toString().isNotEmpty == true
-                          ? experiment['name']
-                          : 'Unnamed Experiment',
+      body: Column(
+        children: [
+          // Sorting Dropdown
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 0, 16, 0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text('Sort experiments by: '),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButton<String>(
+                    value: _selectedSortOption,
+                    elevation: 16,
+                    style: const TextStyle(color: Colors.black),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.black,
                     ),
-                    subtitle: Text(formattedDate),
-                    onTap: () {
-                      // Navigate to a detailed screen or display observations
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ExperimentDetailsScreen(
-                            experiment: experiment,
+                    onChanged: (String? newValue) {
+                      if (newValue != null) {
+                        _sortExperiments(newValue);
+                      }
+                    },
+                    items: <String>[
+                      'Date: recent first',
+                      'Date: oldest first',
+                      'Name: a-z',
+                      'Name: z-a',
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // List of experiments
+          experiments.isEmpty
+              ? const Expanded(
+                  child: Center(
+                    child: Text('No experiments saved yet'),
+                  ),
+                )
+              : Expanded(
+                  child: ListView.builder(
+                    itemCount: experiments.length,
+                    itemBuilder: (context, index) {
+                      final experiment = experiments[index];
+                      final date = DateTime.parse(experiment['date']);
+                      final formattedDate =
+                          DateFormat('yyyy-MM-dd HH:mm').format(date);
+
+                      return Card(
+                        child: ListTile(
+                          title: Text(
+                            experiment['name']?.toString().isNotEmpty == true
+                                ? experiment['name']
+                                : 'Unnamed Experiment',
                           ),
+                          subtitle: Text(formattedDate),
+                          onTap: () {
+                            // Navigate to a detailed screen or display observations
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ExperimentDetailsScreen(
+                                  experiment: experiment,
+                                ),
+                              ),
+                            );
+                          },
                         ),
                       );
                     },
                   ),
-                );
-              },
-            ),
-    );
-  }
-}
-
-// Placeholder for ExperimentDetailsScreen
-class ExperimentDetailsScreen extends StatelessWidget {
-  final Map<String, dynamic> experiment;
-
-  const ExperimentDetailsScreen({Key? key, required this.experiment})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final List<dynamic> observations = experiment['observations'] ?? [];
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(experiment['name'] ?? 'Experiment Details'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Date of experiment: ${experiment['date']}',
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Observations:',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: observations.length,
-                itemBuilder: (context, index) {
-                  final observation = observations[index];
-                  return ListTile(
-                    title:
-                        Text('Concentration: ${observation['concentration']}'),
-                    subtitle: Text('Absorbance: ${observation['absorbance']}'),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+                ),
+        ],
       ),
     );
   }
